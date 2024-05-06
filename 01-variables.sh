329f98d6cc63dbc19fa166055f0b726f8488e88c
Atchula <hanumaiah061@gmail.com>
2024-05-06 18:42:00 +0530

    variablev3

--- 01-variables.sh
+++ 01-variables.sh
@@ -4,6 +4,7 @@
 # Variable Declarations
 PERSON1="Hanu"
 PERSON2="Rupa"
+# Variable Refering
 echo "${PERSON1} : How Are You?"
 echo "${PERSON2} : I am Fine..How Are You?"
 echo "${PERSON1}  : I am Fine How Is Your Coaching"
