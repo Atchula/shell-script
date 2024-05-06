33de571a33864fe7c59ad1ea2cea0cd26ea96e37
Atchula <hanumaiah061@gmail.com>
2024-05-06 22:47:31 +0530

    spev2

---
+++ condition.sh
@@ -0,0 +1,15 @@
+#######################
+## Author : Hanu
+## Date   : 06-05-2024
+## Desc   : Check User Is Root Or Not
+##########################
+#!/bin/bash
+
+# Checking User Is Root Or Not
+CURRENTUSER=${USER}
+if [ ${CURRENTUSER} -eq root ]
+then
+    echo "User Is Root"
+else
+    echo "User Is Not Root"
+fi
