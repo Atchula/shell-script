157322f0d8a2dedd0ed04a6ed70a54f0b45e2758
Atchula <hanumaiah061@gmail.com>
2024-05-06 18:25:05 +0530

    variables v1

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,1 +1,6 @@
-# This Is Variables Script
+#!/bin/bash
+# This Is Variables
+echo "Hanu : How Are You?"
+echo "Rupa : I am Fine..How Are You?"
+echo "Hanu  : I am Fine How Is Your Coaching"
+echo "Rupa : Good.."
