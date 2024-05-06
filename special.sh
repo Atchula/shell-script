e9a04c5a987e10e89b0add726795ce33ed79214f
Atchula <hanumaiah061@gmail.com>
2024-05-06 22:27:09 +0530

    spev2

--- special.sh
+++ special.sh
@@ -1,7 +1,37 @@
-##########################
+##################################
 ## Author : Hanu
-##########################
+## Date   : 06-05-2024
+##################################
 #!/bin/bash
- echo "Process Id Of Current Script : ${$}"
- sleep 60 &
- echo "process id previous background : ${!}"
+
+## Special Variables
+
+# Print All Args
+echo "All Args: ${@}"
+
+# Print No Of Args
+echo "Number Of Args: ${#}"
+
+# Print CWD
+echo "CWD is: ${PWD}"
+
+# Print HOME 
+echo "HOME is :${HOME}"
+
+# print USER RUNNING
+echo "USER Running : ${USER}"
+
+# Print Script Name
+echo "Script : ${0}"
+
+# Print Process
+echo "Process : ${$}"
+
+sleep 60 &
+
+# Print Previous
+echo "Background : ${!}"
+
+# Execute Status
+echo "Execute : ${?}"
+
