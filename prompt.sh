eee1fb065b60e03cb4834b5817faf8ce6d0fccf2
Atchula <hanumaiah061@gmail.com>
2024-05-06 21:26:39 +0530

    promptv2

--- prompt.sh
+++ prompt.sh
@@ -2,14 +2,9 @@
 ## Author : Hanu    
 ## Date : 06-05-2024   
 ######################
-
 #!/bin/bash
-
 # Prompt For UserName and PassWord
-read -p "Login As ::" USERNAME
-read -p "Password ::" PASSWORD
-
-# Print Login Info
-echo "LogIn Details Are : ${USERNAME} ${PASSWORD}"
-
-
+read -p "Login As :" USERNAME
+read -ps "Password :" PASSWORD
+# Print
+echo "Login Details Are : ${USERNAME}.... ${PASSWORD}"
