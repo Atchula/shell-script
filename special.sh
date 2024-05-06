9d431dc7dd8a10e03cf16ec9419caa78ed2dd898
Atchula <hanumaiah061@gmail.com>
2024-05-06 22:07:07 +0530

    spev2

--- special.sh
+++ special.sh
@@ -3,8 +3,11 @@
 ##########################
 #!/bin/bash
 
-# Print All Args
-echo "All Args Are : ${@}"
+# # Print All Args
+# echo "All Args Are : ${@}"
 
-# Number Of Args Passed
-echo "Number Of Args : ${#}"
+# # Number Of Args Passed
+# echo "Number Of Args : ${#}"
+
+# Print Script Name
+echo "Current Script Name : ${0}"
