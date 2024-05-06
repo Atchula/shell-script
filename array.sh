9eea6cc9149b68af7e8df3940a0d42a0d087876a
Atchula <hanumaiah061@gmail.com>
2024-05-06 21:42:27 +0530

    Array

---
+++ array.sh
@@ -0,0 +1,16 @@
+####################
+## Author : Hanu
+###################
+#!/bin/bash
+
+# Declaring Menu
+menu = ("IDLY" "DOSA" "SAMBAR" "CHUTNEY")
+
+# Printing First Item
+echo "First Item Is : ${menu[0]}"
+
+# Printing Last Item
+echo "Last Item Is : ${menu[3]}"
+
+# Printing All Items
+echo "All Items Are : ${menu[@]}"
