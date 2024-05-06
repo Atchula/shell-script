5127be9298e6404c09b72d9516c372f2e2e492f5
Atchula <hanumaiah061@gmail.com>
2024-05-06 18:51:48 +0530

    args

---
+++ args.sh
@@ -0,0 +1,14 @@
+#################
+## Author : Hanu
+#################
+
+#!/bin/bash
+
+# Declare Variables
+PERSON1=${1}
+PERSON2=${2}
+
+# Refering Variables
+
+echo "Hello : ${PERSON1}"
+echo "Hello : ${PERSON2}"
