34c28131b4c54d72fd254725859b6e6a854df2c8
Atchula <hanumaiah061@gmail.com>
2024-05-06 21:52:00 +0530

    Datatype

---
+++ datatypes.sh
@@ -0,0 +1,15 @@
+###########################
+## Author : Hanu
+###########################
+#!/bin/bash
+
+# Prompt For Two Numbers
+read -p "Enter FistNumber:" F1
+read -p "Enter SecondNumber" F2
+
+# Sum Of Two Numbers
+sum=$((F1+F2))
+
+# Print
+echo "Sum Is : ${sum}"
+
