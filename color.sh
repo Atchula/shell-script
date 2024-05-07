e1ec376aefe3339e3c4520cd5899e51a1367c237
Atchula <hanumaiah061@gmail.com>
2024-05-07 15:49:19 +0530

    sh

---
+++ color.sh
@@ -0,0 +1,37 @@
+###################################
+## Author : Hanu
+## Date   : 07-05-2024
+###################################
+#!bin/bash
+
+# COLORS 
+
+red="\e[31m"
+green="\e[32m"
+
+# Get User Details
+USER=$(id -u)  
+
+# Function To Check User Admin or Not
+VALIDATE_USER()
+{
+
+if [ ${USER} == 0 ]
+then 
+  echo -e "${green} Admin,Proceed"
+else
+  echo -e "${red} Running without previliges,Contact Admin"
+  exit 1
+fi
+
+}
+
+VALIDATE_PACKAGE()
+{
+
+}
+
+
+# # Install Package 
+# dnf install nginx -y 
+
