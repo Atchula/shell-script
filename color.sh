0f65b46fe8e0bff1451bcde654c9a2c61c35dd07
Atchula <hanumaiah061@gmail.com>
2024-05-07 16:55:23 +0530

    sh

--- color.sh
+++ color.sh
@@ -12,6 +12,8 @@ green="\e[32m"
 # Get User Details
 USER=$(id -u)  
 
+LOGFILE="/tmp/$(echo $0 | cut -d "." -f1)-$(date +%F-%H-%M-%S).log"
+
 # Function To Check User Admin or Not
 VALIDATE_USER()
 {
@@ -29,12 +31,23 @@ fi
 # Calling Validate User
 VALIDATE_USER
 
-# VALIDATE_PACKAGE()
-# {
+ VALIDATE_PACKAGE()
+{
 
-# }
+   if [ $1 == 0 ] 
+   then 
+     echo -e  "${red} $2 Installed Success"  
+  else
+     echo -e "${green} $2 Installed Failure"
+     exit 1
+   fi
+
+}
 
 
 # # Install Package 
-# dnf install nginx -y 
+ dnf install nginx -y  &>> LOGFILE
+
+ VALIDATE_PACKAGE
+
 
