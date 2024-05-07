eb6aa05ae304c8d196c111c59205feeea77af4ca
Atchula <hanumaiah061@gmail.com>
2024-05-07 13:40:51 +0530

    sh

---
+++ function.sh
@@ -0,0 +1,27 @@
+#######################################
+### Author : Hanu
+### Date   : 07-05-2024
+#######################################
+#!/bin/bash
+
+# Getting User Details
+USER = $(id -u)
+
+# Declaring Function To Validate
+VALIDATE()
+{
+    if [ $1 == 0 ]
+    then 
+       echo "Admin,Proceed"
+    else
+       echo "Run with Admin Access!!!!"
+       exit 1
+}
+
+# Calling Validate Function To Check User Admin Or NOt
+VALIDATE ${USER}
+
+
+
+
+
