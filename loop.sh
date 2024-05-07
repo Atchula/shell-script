96f26a134710b4a68cbf99d56408d6cd61f009b2
Atchula <hanumaiah061@gmail.com>
2024-05-07 17:47:25 +0530

    sh

--- loop.sh
+++ loop.sh
@@ -4,9 +4,34 @@
 #############################################
 #!/bin/bash
 
-## Loop
+# Colors
 
-for i in {1..20}
+red="\e[31m"
+green="\e[32m"
+normal="\e[0m"
+
+# Declare Packages
+packages=("mysql" "nginx" "mysql-server")
+
+# Create LOGFILE
+LOGILE="/tmp/$(echo $0 | cut -d "." -f1)-$(date +%F-%H-%M-%S).log"
+
+# Checking Package
+
+validate_package()
+{
+if [ $1 == 0]
+then 
+  echo -e "$red $2 Installation Success $normal"
+else
+  echo -e "$green $2 Installation Failure $normal" 
+fi
+
+}
+
+
+for item in ${packages[@]}
 do
-echo "$i"
+dnf install $item -y &>> $LOGFILE
+validate_package $? "$item"
 done
